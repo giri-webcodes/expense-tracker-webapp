39ce105ad92c267c1e151c99562b978463c8e686
Giri Prasad S <51823589+giri-webcodes@users.noreply.github.com>
2022-12-13 16:52:24 +0530

    Added dropdown filter - expenselist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExpenseListComponent } from './expense-list/expense-list.component';
@@ -14,7 +14,8 @@ import { EditComponent } from './edit/edit.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/expense-list/expense-list.component.ts
+++ src/app/expense-list/expense-list.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { expenseList } from '../expenselist';
+import { monthList } from '../expenselist';
+import { yearList } from '../expenselist';
+import { Expense } from '../expenselist';
 
 @Component({
   selector: 'app-expense-list',
   templateUrl: './expense-list.component.html',
   styleUrls: ['./expense-list.component.css']
 })
-export class ExpenseListComponent {
+
+export class ExpenseListComponent implements OnInit {
    expenseList=expenseList;
- 
+   monthList=monthList;
+   yearList=yearList;
+   totalExpense:number=0;
+   filterList:Expense[]=[];
+
+   public selectedMonth:number=0;
+   public selectedYear:number=0;
+
+   ngOnInit(){    
+    this.selectedMonth=new Date().getMonth()+1;
+    this.selectedYear=new Date().getFullYear();       
+   }
+
+      
+   public getData(){
+            
+    this.filterList=[];
+    this.expenseList=expenseList;
+    this.totalExpense=0;
+    
+    this.expenseList.forEach(x=>{
+      var dt = new Date(x.date);            
+      if(Number(dt.getMonth()+1) == this.selectedMonth && dt.getFullYear() == this.selectedYear)
+      {                
+        this.filterList.push(x);  
+        this.totalExpense=this.totalExpense+x.amount;      
+      }      
+    });
+
+    this.expenseList=this.filterList;     
+   }      
 }
