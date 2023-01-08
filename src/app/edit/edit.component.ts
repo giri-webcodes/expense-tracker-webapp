3207fecb481ed214a45cb931ac8a2945e95bf221
Giri Prasad S <51823589+giri-webcodes@users.noreply.github.com>
2023-01-08 17:06:27 +0530

    created edit form

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,5 +1,6 @@
-import { Component,OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Expense } from '../expenselist';
 import { expenseList } from '../expenselist';
 
@@ -10,16 +11,33 @@ import { expenseList } from '../expenselist';
 })
 export class EditComponent implements OnInit {
 
- expense!:Expense;
+  expenseobj: Expense;
+  expenseForm: FormGroup;
 
- constructor(private route:ActivatedRoute){}
+  constructor(private route: ActivatedRoute, private fb: FormBuilder, private router: Router) {
+  }
 
   ngOnInit(): void {
-   const routeParams = this.route.snapshot.paramMap;
-   const id = Number(routeParams.get('expenseId'));
-   let jsonStr = JSON.stringify(expenseList.find(x=>x.id==id));
-   let json = JSON.parse(jsonStr);
-     this.expense= json as Expense;
+    const routeParams = this.route.snapshot.paramMap;
+    const id = Number(routeParams.get('expenseId'));
+    let jsonStr = JSON.stringify(expenseList.find(x => x.id == id));
+    let json = JSON.parse(jsonStr);
+    this.expenseobj = json as Expense;
+
+    this.expenseForm = this.fb.group({
+      expense: new FormControl([this.expenseobj.expense]),
+      amount: new FormControl([this.expenseobj.amount]),
+      date: new FormControl([new Date(this.expenseobj.date)]),
+      comment: new FormControl([this.expenseobj.comment])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.expenseForm.value);
+  }
+
+  goToHome(){
+    this.router.navigate(['/']);
   }
 
 }
