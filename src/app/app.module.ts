2c5a3765009cf46769257b92736ab8b363c802b1
Giri Prasad S <51823589+giri-webcodes@users.noreply.github.com>
2023-01-12 15:22:55 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {MatTableModule} from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatInputModule} from '@angular/material';
 
 @NgModule({
   declarations: [
